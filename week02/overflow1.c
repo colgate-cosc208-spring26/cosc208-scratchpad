a2e5211b86da9c6cf4fe873f8db116fce0542380
Joel Sommers <jsommers@colgate.edu>
2026-01-31 16:05:22 -0500

    add overflow code

---
+++ week02/overflow1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    unsigned char c = 300;
+    printf("%u\n", c);
+}
+
---
+++ week02/overflow2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    unsigned int i = 300;
+    unsigned char c = i;
+    printf("%u\n", c);
+}
+
